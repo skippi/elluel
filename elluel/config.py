070c9ffae630020f3fcc0c4fa0e024132171306c
skippi <jbtcao@gmail.com>
2019-09-08 11:03:48 -0500

    Added hypothesis dependency

--- elluel/config.py
+++ elluel/config.py
@@ -1,18 +1,22 @@
 import json
 from dataclasses import dataclass
-from typing import (Dict, Any)
+from typing import Dict, Any
+
 
 @dataclass
 class Config:
     pass
 
+
 def parse(_: str) -> Config:
     return Config()
+
 
 @dataclass
 class BgmInfo:
     description: str
+    filename: str
 
     @classmethod
     def from_dict(cls, d: Dict[str, Any]):
-        return BgmInfo(description=d["description"])
+        return BgmInfo(description=d.get("description", ""), filename=d.get("filename", ""))
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,11 +1,20 @@
-from elluel.config import (BgmInfo, Config, parse)
+from elluel.config import BgmInfo, Config, parse
+from hypothesis import given
+from hypothesis.strategies import fixed_dictionaries, text, tuples, just, lists
 from pytest import raises
 
 
 def test_parse_returns_default_given_empty_object_str():
     assert parse("{}") == Config()
 
+
+_BgmInfoKV = tuples(just("description"), text()) | tuples(just("filename"), text())
+_BgmInfoData = lists(_BgmInfoKV).map(dict)
+
+
 class TestBgmInfo:
-    def test_from_dict_reads_description(self):
-        info = BgmInfo.from_dict({"description": "Kerning Square"})
-        assert info.description == "Kerning Square"
+    @given(obj=_BgmInfoData)
+    def test_from_dict(self, obj):
+        info = BgmInfo.from_dict(obj)
+        assert info.description == obj.get("description", "")
+        assert info.filename == obj.get("filename", "")
